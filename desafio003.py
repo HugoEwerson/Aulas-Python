65f068d307f6fea37ca87deffdc5e37f39761469
Hugo Ewerson <hugo.ewerson@gmail.com>
2024-12-13 04:15:55 +0000

    Terceiro Commit

--- desafio003.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input('Digite um numero entre 0 e 100 »'))
-num2 = int(input('Digite mais um numero entre 0 e 100 »'))
-soma = num1 + num2
-print('A soma entre o numero {} e o {} e {}'.format(num1,num2,soma))
